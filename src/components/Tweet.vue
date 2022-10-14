f0f70e82d98ca99832a55d988beae9c2c930d142
PaulaSousa2014 <110301198+PaulaSousa2014@users.noreply.github.com>
2022-10-14 07:25:22 +0200

    Iteración 5

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,47 +1,46 @@
 <template>
-    <div class="tweet">
-        <ProfileImage :image="user.image" />
-        
-        
-        <div class="body">
-            <div class="top">
-                <span class="user">
-                    <span class="name">{{props.name}}</span>
-                    <span class="handle">{{props.handle}}</span>
-                </span>
+  <div class="tweet">
+    <ProfileImage :image="user.image" />
 
-                <span class="timestamp">{{props.timestamp}}</span>
-            </div>
+    <div class="body">
+      <div class="top">
+        <User userData="user" :name="props.name" :handle="props.handle"/>
+        <Timestamp :timestamp="props.timestamp"/>
 
-            <p class="message">
-                {{props.message}}
-            </p>
+        <span class="timestamp">{{ props.timestamp }}</span>
+      </div>
 
-            <div class="actions">
-                <!-- Font Awesome icons -->
-                <i class="far fa-comment"></i>
-                <i class="fas fa-retweet"></i>
-                <i class="far fa-heart"></i>
-                <i class="fas fa-share"></i>
-            </div>
-        </div>
+      <p class="message">
+        {{ props.message }}
+      </p>
 
-        <i class="fas fa-ellipsis-h"></i>
+      <div class="actions">
+        <!-- Font Awesome icons -->
+        <i class="far fa-comment"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="far fa-heart"></i>
+        <i class="fas fa-share"></i>
+      </div>
     </div>
+
+    <i class="fas fa-ellipsis-h"></i>
+  </div>
 </template>
 <script setup>
-const props = defineProps({
-    name: String,
-    image: String,
-    handle: String,
-    timestamp: String,
-    message: String
-})
+import ProfileImage from './ProfileImage.vue'
+import User from './/User.vue'
 
+const props = defineProps({
+  name: String,
+  image: String,
+  handle: String,
+  timestamp: String,
+  message: String,
+});
 </script>
-  
+
 <style scoped>
 a {
-    color: #42b983;
+  color: #42b983;
 }
 </style>
